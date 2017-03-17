19136e1bbf28c87690e0236a15000136c4282b21
haoshenyang <hao-shen@qq.com>
2017-03-17 11:41:44 +0800

    BinaryTree

---
+++ BinaryTree.swift
@@ -0,0 +1,54 @@
+
+import Foundation
+
+
+indirect enum BinaryTree<T> {
+    case node(BinaryTree<T>, T, BinaryTree<T>)
+    case empty
+}
+
+extension BinaryTree: CustomStringConvertible {
+    
+    var description: String {
+        switch self {
+        case let .node(left, value, right):
+            return "value: \(value), left = [\(left.description)], right = [\(right.description)]"
+        case .empty:
+            return ""
+        }
+    }
+    
+    var count: Int {
+        switch self {
+        case let .node(left, _, right):
+            return left.count + 1 + right.count
+        case .empty:
+            return 0
+        }
+    }
+    
+    func traverseInOrder(process: (T) -> Void) {
+        if case let .node(left, value, right) = self {
+            left.traverseInOrder(process: process)
+            process(value)
+            right.traverseInOrder(process: process)
+        }
+    }
+    
+    func traversePreOrder(process: (T) -> Void) {
+        if case let .node(left, value, right) = self {
+            process(value)
+            left.traversePreOrder(process: process)
+            right.traversePreOrder(process: process)
+        }
+    }
+    
+    func traversePostOrder(process: (T) -> Void) {
+        if case let .node(left, value, right) = self {
+            left.traversePostOrder(process: process)
+            right.traversePostOrder(process: process)
+            process(value)
+        }
+    }
+}
+
